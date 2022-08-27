b8f2b75763f688d1b149b306b171fa7cb72eec6d
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-08-27 11:56:51 +0900

    [Feat] 퀸구현

--- vrTest/Assets/ChaseToPlayer.cs
+++ vrTest/Assets/ChaseToPlayer.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class ChaseToPlayer : MonoBehaviour
 {
     [SerializeField] float speed = 2f;
-    [SerializeField] float waitTime = 3f;
+    [SerializeField] float waitTime = 3f, dashTime = 1f;
     [SerializeField] Transform target;
     [SerializeField] float dashLength = 7f;
     [SerializeField] ParticleSystem ps;
     Renderer renderer;
     [SerializeField] Vector3 startPos;
-
+    Rigidbody rb;
     public bool reStart;
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();  
         startPos = transform.position;
     }
     private void Start()
@@ -26,50 +27,48 @@ public class ChaseToPlayer : MonoBehaviour
         StartCoroutine(WaitForMoveMent());
     }
     float absorbTime = 3f;
-    float p = 0f;
 
     bool isEnter;
     IEnumerator WaitForMoveMent()
     {
-
-        p = 0f;
+        yield return new WaitForSeconds(waitTime);
         ps.gameObject.SetActive(true);
+
         int count = renderer.material.GetInt("_FinalPower");
         count = 1;
+        float p = 0;
+
+        Vector3 dir = (target.position - transform.position).normalized;
+        Quaternion cq = transform.rotation;
+        Quaternion eq = Quaternion.LookRotation(dir, Vector3.up);
         while (p < absorbTime)
         {
             p += Time.deltaTime;
             renderer.material.SetInt("_FinalPower", count + 1);
+            transform.rotation = Quaternion.Slerp(cq, eq, Mathf.Clamp01(p / absorbTime * 3f));
             yield return null;
         }
         ps.gameObject.SetActive(false);
 
-        //transform.forward = dir;
 
-        yield return new WaitForSeconds(2f);
-
-        Vector3 dir = (target.position - transform.position).normalized;
-        t = 0f;
         targetPos = transform.position + dir * dashLength;
         originPos = this.transform.position;
         isOnce = true;
-
-        while (t < waitTime)
+        float t = 0;
+        while (t < 2f)
         {
-
-            t += Time.deltaTime;
-            transform.position = Vector3.Lerp(this.transform.position, targetPos, (t / waitTime));
             yield return null;
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, targetPos, (t / dashTime) * Time.deltaTime * 2f);
+            transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(-90f, 90f, 90f), (t / dashTime) * Time.deltaTime * 2f);
         }
-
         renderer.material.SetFloat("_FinalPower", 1f);
-        yield return new WaitForSeconds(waitTime);
+        
         StartCoroutine(WaitForMoveMent());
     }
     float timer = 0f;
     bool isOnce;
     Vector3 targetPos;
-    float t;
     Vector3 originPos;
 
 
@@ -77,7 +76,7 @@ public class ChaseToPlayer : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            StopCoroutine(WaitForMoveMent());
+            StopAllCoroutines();
             other.GetComponent<ReStart>().ReStartGame();
 
             ReStart();
@@ -86,7 +85,10 @@ public class ChaseToPlayer : MonoBehaviour
 
     void ReStart()
     {
-        this.transform.rotation = Quaternion.Euler(90f, 90f, 90f);
+        this.transform.rotation = Quaternion.Euler(-90f, 90f, 90f);
         this.transform.position = startPos; //왜안되지?
+        ps.gameObject.SetActive(false);
+        Debug.Log("RESTART");
+        StartCoroutine(WaitForMoveMent());
     }
 }
