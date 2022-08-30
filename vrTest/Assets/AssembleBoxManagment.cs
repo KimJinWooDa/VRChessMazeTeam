ca2f9de3c146c1d105496fb687553a10530abd07
Sunny Kim <starwin@gmail.com>
2022-08-30 19:13:54 +0900

    key & box sound

--- vrTest/Assets/AssembleBoxManagment.cs
+++ vrTest/Assets/AssembleBoxManagment.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class AssembleBoxManagment : MonoBehaviour
 {
     [SerializeField] GameObject[] assembleBoxs;
+    private bool ended = false;
+
+    private void Awake() {
+        ended = false;
+    }
 
     public void Assemble()
     {
         foreach (var item in assembleBoxs)
         {
             item.GetComponent<AssembleGroundBox>().isAssemble = true;
+        }
+        if (!ended) {
+            ended = true;
+            AudioSource s;
+            if(TryGetComponent(out s)) s.Play();
         }
     }
 }
--- vrTest/Assets/AssembleGroundBox.cs
+++ vrTest/Assets/AssembleGroundBox.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class AssembleGroundBox : MonoBehaviour
 {
     [SerializeField] Vector3 targetMovePosition;
+    private AudioSource audios;
 
     public bool isAssemble;
+    private bool ended = false;
 
     [SerializeField] float speed = 1.5f;
+
+    private void Awake() {
+        ended = false;
+        audios = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
-       if(isAssemble) transform.position = Vector3.MoveTowards(this.transform.position, targetMovePosition, Time.deltaTime * speed);
+        if (isAssemble) {
+            transform.position = Vector3.MoveTowards(transform.position, targetMovePosition, Time.deltaTime * speed);
+            if(!ended && (transform.position - targetMovePosition).sqrMagnitude < 0.0001f) {
+                ended = true;
+                audios.Play();
+            }
+        }
     }
 }
